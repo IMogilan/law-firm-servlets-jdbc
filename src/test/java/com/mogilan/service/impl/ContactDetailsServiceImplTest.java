cbac2daf8693443ae2899c20121b69260b4b357c
Ilya Mogilan <ilya.mogilan@gmail.com>
2023-11-23 15:10:34 +0300

    LAW_FIRM/ Added ContactDetailsServiceImplTest.

--- src/test/java/com/mogilan/service/impl/ContactDetailsServiceImplTest.java
+++ src/test/java/com/mogilan/service/impl/ContactDetailsServiceImplTest.java
@@ -1,30 +1,37 @@
 package com.mogilan.service.impl;
 
 import com.mogilan.exception.EntityNotFoundException;
+import com.mogilan.model.ContactDetails;
 import com.mogilan.repository.ContactDetailsDao;
 import com.mogilan.service.ContactDetailsService;
 import com.mogilan.servlet.dto.ContactDetailsDto;
 import com.mogilan.servlet.mapper.ContactDetailsMapper;
-import com.mogilan.servlet.mapper.impl.ContactDetailsMapperImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ContactDetailsServiceImplTest {
 
     @Mock
     ContactDetailsDao contactDetailsDao;
-    ContactDetailsMapper contactDetailsMapper = new ContactDetailsMapperImpl();
+    @Mock
+    ContactDetailsMapper contactDetailsMapper;
     ContactDetailsService contactDetailsService;
+
+    @Captor
+    ArgumentCaptor<ContactDetails> captor;
 
 
     @BeforeEach
@@ -34,6 +41,21 @@ class ContactDetailsServiceImplTest {
 
     @Test
     void createSuccess() {
+        var dto = getDto();
+        var entity = getEntity();
+        doReturn(entity).when(contactDetailsMapper).toEntity(dto);
+
+        var entityWithId = getEntityWithId();
+        doReturn(entityWithId).when(contactDetailsDao).save(entity);
+
+        var dtoWithId = getDtoWithId();
+        doReturn(dtoWithId).when(contactDetailsMapper).toDto(entityWithId);
+
+        var actualResult = contactDetailsService.create(dto);
+
+        assertThat(actualResult).isNotNull();
+        assertThat(actualResult.getId()).isNotNull();
+        assertThat(actualResult.getId()).isEqualTo(dtoWithId.getId());
     }
 
     @Test
@@ -43,11 +65,29 @@ class ContactDetailsServiceImplTest {
     }
 
     @Test
-    void readAll() {
+    void readAllSuccess() {
+        var entityList = getEntityList();
+        doReturn(entityList).when(contactDetailsDao).findAll();
+        var dtoList = getDtoList();
+        doReturn(dtoList).when(contactDetailsMapper).toDtoList(entityList);
+
+        var actualResult = contactDetailsService.readAll();
+        assertThat(actualResult).isNotNull();
+        assertThat(actualResult).isNotEmpty();
+        assertThat(actualResult).hasSize(entityList.size());
     }
 
     @Test
     void readById() {
+        Long id = 1L;
+        var entityWithId = getEntityWithId();
+        var dtoWithId = getDtoWithId();
+        doReturn(Optional.of(entityWithId)).when(contactDetailsDao).findById(id);
+        doReturn(dtoWithId).when(contactDetailsMapper).toDto(entityWithId);
+
+        var actualResult = contactDetailsService.readById(id);
+        assertThat(actualResult).isNotNull();
+        assertThat(actualResult.getId()).isEqualTo(id);
     }
 
     @Test
@@ -64,7 +104,19 @@ class ContactDetailsServiceImplTest {
     }
 
     @Test
-    void update() {
+    void updateSuccess() {
+        Long id = 1L;
+        var entity = getEntity();
+        var dto = getDto();
+        doReturn(entity).when(contactDetailsMapper).toEntity(dto);
+        doReturn(Optional.of(entity)).when(contactDetailsDao).findById(id);
+
+        contactDetailsService.update(id, dto);
+
+        verify(contactDetailsDao, times(1)).update(captor.capture());
+        var value = captor.getValue();
+        assertThat(value).isNotNull();
+        assertThat(value.getId()).isEqualTo(id);
     }
 
     @Test
@@ -90,7 +142,14 @@ class ContactDetailsServiceImplTest {
     }
 
     @Test
-    void deleteById() {
+    void deleteByIdSuccess() {
+        Long id = 1L;
+        var entity = getEntity();
+        doReturn(Optional.of(entity)).when(contactDetailsDao).findById(id);
+
+        contactDetailsService.deleteById(id);
+
+        verify(contactDetailsDao, times(1)).delete(id);
     }
 
     @Test
@@ -104,5 +163,29 @@ class ContactDetailsServiceImplTest {
         Long id = 1000L;
         doReturn(Optional.empty()).when(contactDetailsDao).findById(id);
         Assertions.assertThrows(EntityNotFoundException.class, () -> contactDetailsService.deleteById(id));
+    }
+
+    private ContactDetailsDto getDtoWithId() {
+        return new ContactDetailsDto(1L, "Address", "777", "888", "999", "test@email.com");
+    }
+
+    private ContactDetails getEntityWithId() {
+        return new ContactDetails(1L, "Address", "777", "888", "999", "test@email.com");
+    }
+
+    private ContactDetails getEntity() {
+        return new ContactDetails("Address", "777", "888", "999", "test@email.com");
+    }
+
+    private ContactDetailsDto getDto() {
+        return new ContactDetailsDto("Address", "777", "888", "999", "test@email.com");
+    }
+
+    private List<ContactDetailsDto> getDtoList() {
+        return List.of(getDtoWithId(),getDtoWithId(),getDtoWithId(),getDtoWithId(),getDtoWithId());
+    }
+
+    private List<ContactDetails> getEntityList() {
+        return List.of(getEntityWithId(),getEntityWithId(),getEntityWithId(),getEntityWithId(),getEntityWithId());
     }
 }
